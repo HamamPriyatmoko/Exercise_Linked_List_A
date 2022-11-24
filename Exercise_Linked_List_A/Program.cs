7a7e56ba437e3854b78966486fa43eeac63fc98a
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-11-24 15:37:48 +0700

    Add project files.

---
+++ Exercise_Linked_List_A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Linked_List_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
