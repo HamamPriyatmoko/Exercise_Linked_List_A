77c22eda283e03e4e7f45a1e40d63b9b667500e3
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-11-24 15:40:46 +0700

    membuat fungtion circularlist yang akan berisi method

--- Exercise_Linked_List_A/Program.cs
+++ Exercise_Linked_List_A/Program.cs
@@ -13,6 +13,10 @@ namespace Exercise_Linked_List_A
         public string name;
         public Node next;
     }
+    class CircularList
+    {
+        Node Last;
+    }
     internal class Program
     {
         static void Main(string[] args)
